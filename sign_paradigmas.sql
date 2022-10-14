830bea661459f816d38be4fe3ad790f16e1a5800
alekelbar <alekelbar.developer@gmail.com>
2022-10-14 02:19:49 -0600

    database modified

--- sign_paradigmas.sql
+++ sign_paradigmas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2022 at 04:54 AM
+-- Generation Time: Oct 14, 2022 at 10:18 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -33,6 +33,16 @@ CREATE TABLE `department` (
   `manager` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`id`, `name`, `manager`) VALUES
+(1, 'TI', NULL),
+(2, ' MCdonald\'s', NULL),
+(3, 'Admin', NULL),
+(4, 'merchandising', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +61,16 @@ CREATE TABLE `employee` (
   `state` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id`, `f_name`, `l_name`, `email`, `phone`, `hire`, `job`, `dept`, `state`) VALUES
+(2, 'Edward', 'Cortés', 'Ed@gmail.com', '12345678', '2022-10-13', 7, 2, 3),
+(3, 'Alexander', 'Barquero', 'Alekelbar.personal@gmail.com', '86307982', '2022-10-13', 6, 1, 1),
+(4, 'Arianny', 'Rosales', 'Ary@gmail.com', '12344567', '2022-09-26', 6, 4, 2),
+(5, 'Mathew', 'Castro', 'Castro@gmail.com', '12345678', '2022-10-13', 7, 2, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -60,23 +80,41 @@ CREATE TABLE `employee` (
 CREATE TABLE `job` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `desc` varchar(50) NOT NULL
+  `desc` varchar(50) NOT NULL,
+  `extra` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `job`
+--
+
+INSERT INTO `job` (`id`, `name`, `desc`, `extra`) VALUES
+(4, 'Designer', 'Visual Designer', 10),
+(5, 'Advertisement', 'merchandising', 5),
+(6, 'Programmer', 'make software', 35),
+(7, 'Secretary', 'Make documentation', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `planilla`
+-- Table structure for table `spreadsheet`
 --
 
-CREATE TABLE `planilla` (
+CREATE TABLE `spreadsheet` (
   `id` int(11) NOT NULL,
   `employee_id` int(11) NOT NULL,
   `sal_bruto` double NOT NULL,
-  `sal_neto` double NOT NULL,
-  `reb_lab` double NOT NULL,
-  `extra` int(11) NOT NULL
+  `extra` int(11) NOT NULL,
+  `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `spreadsheet`
+--
+
+INSERT INTO `spreadsheet` (`id`, `employee_id`, `sal_bruto`, `extra`, `date`) VALUES
+(12, 2, 1500, 12, '2022-10-14'),
+(13, 3, 2000, 5, '2022-10-25');
 
 -- --------------------------------------------------------
 
@@ -89,6 +127,15 @@ CREATE TABLE `state` (
   `name` varchar(50) NOT NULL,
   `desc` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `state`
+--
+
+INSERT INTO `state` (`id`, `name`, `desc`) VALUES
+(1, 'Hired', 'Hired staff'),
+(2, 'SubContract', 'SubContract staff'),
+(3, 'Permanent', 'Permanent Staff');
 
 --
 -- Indexes for dumped tables
@@ -115,9 +162,9 @@ ALTER TABLE `job`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `planilla`
+-- Indexes for table `spreadsheet`
 --
-ALTER TABLE `planilla`
+ALTER TABLE `spreadsheet`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_employee` (`employee_id`);
 
@@ -135,31 +182,31 @@ ALTER TABLE `state`
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `job`
 --
 ALTER TABLE `job`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `planilla`
+-- AUTO_INCREMENT for table `spreadsheet`
 --
-ALTER TABLE `planilla`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `spreadsheet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `state`
 --
 ALTER TABLE `state`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -178,9 +225,9 @@ ALTER TABLE `employee`
   ADD CONSTRAINT `fk_dept` FOREIGN KEY (`dept`) REFERENCES `department` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `planilla`
+-- Constraints for table `spreadsheet`
 --
-ALTER TABLE `planilla`
+ALTER TABLE `spreadsheet`
   ADD CONSTRAINT `fk_employee` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
